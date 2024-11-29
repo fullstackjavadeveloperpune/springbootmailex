5e0647aad6cee56610067a5bfbbfd8debe7ebdfb
Rupesh-Nighojkar <rupeshnighojkar2023@gmail.com>
2024-11-29 09:57:57 +0530

    Added the Http Status code.

--- springbootemailex/src/main/java/com/fullstack/controller/EmailController.java
+++ springbootemailex/src/main/java/com/fullstack/controller/EmailController.java
@@ -4,6 +4,7 @@ import com.fullstack.model.EmailModel;
 import com.fullstack.service.IEmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ public class EmailController {
         log.info("########Sending Email CC: " + emailModel.getCcEmail().toString());
         emailService.sendEmail(emailModel);
 
-        return ResponseEntity.ok("Mail Sent Successfully");
+        return new ResponseEntity<>("Mail Sent Successfully!", HttpStatus.OK);
+
     }
 }
